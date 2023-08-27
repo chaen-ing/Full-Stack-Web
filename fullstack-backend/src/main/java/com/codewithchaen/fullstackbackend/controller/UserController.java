f805ad468ef6db418ba8fe42a1382ba728abad3b
chaen-ing <96906242+chaen-ing@users.noreply.github.com>
2023-08-27 19:37:20 +0900

    Display Information using AXIOS GET

--- fullstack-backend/src/main/java/com/codewithchaen/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/codewithchaen/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.codewithchaen.fullstackbackend.controller;
 import com.codewithchaen.fullstackbackend.model.User;
 import com.codewithchaen.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
