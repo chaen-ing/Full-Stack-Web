8122cdb682090a7af467e298439c51d0ee794936
chaen-ing <96906242+chaen-ing@users.noreply.github.com>
2023-09-10 20:10:31 +0900

    Create Register User Form using React

--- fullstack-front/src/users/AddUser.js
+++ fullstack-front/src/users/AddUser.js
@@ -1,5 +1,53 @@
 import React from 'react'
 
+// 유저 등록 페이지
 export default function AddUser() {
-  return <div>AddUser Form</div>;
+  return <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3 border rounded p-4 mt shadow">
+        <h2 className="text-center m-4">Register User</h2>
+        {/* name */}
+        <div className="mb-3">
+          <label htmlFor="Name" className="form-label">
+            Name
+          </label>
+          <input
+          type = {"text"}
+          className="form-control"
+          placeholder="Enter your name"
+          name="name"/>
+        </div>
+        {/* username */}
+        <div className="mb-3">
+          <label htmlFor="Username" className="form-label">
+            Username
+          </label>
+          <input
+          type = {"text"}
+          className="form-control"
+          placeholder="Enter your username"
+          name="username"/>
+        </div>
+        {/* email */}
+        <div className="mb-3">
+          <label htmlFor="Email" className="form-label">
+            E-mail
+          </label>
+          <input
+          type = {"text"}
+          className="form-control"
+          placeholder="Enter your e-mail address"
+          name="email"/>
+        </div>
+        {/* 제출버튼 */}
+        <button type="submit" className="btn btn-outline-primary">
+          Submit
+        </button>
+        {/* 취소버튼 */}
+        <button type="submit" className="btn btn-outline-danger mx-2">
+          Cancel
+        </button>
+      </div>
+    </div>
+  </div>;
 }
